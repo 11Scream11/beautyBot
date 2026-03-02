fee78433ee2e8c9559eb570d0793a424cbacad69
Abdu17111 <Abdufree245@gmail.com>
2026-03-02 19:59:53 +0600

    Delete utils.py

--- utils.py
+++
@@ -1,3 +0,0 @@
-def is_greeting(text):
-    greetings = ["привет", "здравствуй", "добрый", "hello", "hi", "доброе", "день", "вечер", "утро", "хай", "салам", "здарова"]
-    return any(g in text.lower() for g in greetings)
